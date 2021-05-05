a5532bb3951903f191f9843ef5855b081afa0aaa
mpmpmpmpm <83619362+mpmpmpmpm@users.noreply.github.com>
2021-05-05 16:59:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-document.getElementById("m-1").href = "https://api.whatsapp.com/send?phone=541156628135&text=¡Hola!%20Quiero%20reservar%20el%Moisés%201";
-document.getElementById("m-2").href ="https://api.whatsapp.com/send?phone=541156628135&text=¡Hola!%20Quiero%20reservar%20el%Moisés%202";
+document.getElementById("m-1").href = "https://api.whatsapp.com/send?phone=541156628135&text=¡Hola!%20Quiero%20reservar%20el%20Moisés%201";
+document.getElementById("m-2").href ="https://api.whatsapp.com/send?phone=541156628135&text=¡Hola!%20Quiero%20reservar%20el%20Moisés%202";
