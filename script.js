4e3adc14654137386626bcb27fe8b01cba622a5e
mpmpmpmpm <83619362+mpmpmpmpm@users.noreply.github.com>
2021-05-05 17:01:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-document.getElementById("m-1").href = "https://api.whatsapp.com/send?phone=541156628135&text=¡Hola!%20Quiero%20reservar%20el%20Moisés%201";
-document.getElementById("m-2").href ="https://api.whatsapp.com/send?phone=541156628135&text=¡Hola!%20Quiero%20reservar%20el%20Moisés%202";
+
